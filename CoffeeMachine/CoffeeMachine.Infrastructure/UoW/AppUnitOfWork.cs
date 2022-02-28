f527ea41389d7d4187058795d5c9b2a74eebd5be
SukharevMA <SukharevMA@tomskasu.ru>
2022-02-28 15:55:24 +0700

    Создание UoW

---
+++ CoffeeMachine/CoffeeMachine.Infrastructure/UoW/AppUnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿namespace CoffeeMachine.Infrastructure.UoW;
+
+using CoffeeMachine.Core.Interfaces.Repositories;
+using CoffeeMachine.Core.Interfaces.UoW;
+using CoffeeMachine.Infrastructure.Data;
+using CoffeeMachine.Infrastructure.Repositories;
+
+/// <inheritdoc cref="IAppUnitOfWork"/>
+public class AppUnitOfWork : IAppUnitOfWork
+{
+    private readonly AppDbContext _context;
+    public ICoffeeRepository Coffee { get; }
+    public IMachineBanknoteRepository MachineBanknotes { get; }
+    public IOrderRepository Orders { get; }
+
+    public AppUnitOfWork(AppDbContext context)
+    {
+        _context = context;
+        Coffee = new CoffeeRepository(context);
+        MachineBanknotes = new MachineBanknoteRepository(context);
+        Orders = new OrderRepository(context);
+    }
+
+    public async Task CommitAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
